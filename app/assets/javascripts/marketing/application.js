0d1f0eafdfb99bed8f865fe2cb59ae050a04e510
Robert Klubenspies <robert.klubenspies@gmail.com>
2013-04-08 16:41:39 -0400

    Marketing almost done, footer left to do.

--- app/assets/javascripts/marketing/application.js
+++ app/assets/javascripts/marketing/application.js
@@ -6,4 +6,15 @@ $(document).foundation();
 
 $(document).ready(function() {
 
+  // Credit: http://www.paulund.co.uk/smooth-scroll-to-internal-links-with-jquery
+  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    e.preventDefault();
+    var target = this.hash;
+      $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 500, 'swing', function () {
+      window.location.hash = target;
+    });
+  });
 });
