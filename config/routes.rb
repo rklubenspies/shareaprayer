f2f13246b469ed9270bb266d4739c7a51c882c12
Robert Klubenspies <robert.klubenspies@gmail.com>
2013-04-08 22:16:19 -0400

    Final marketing stuff. Production route protection.

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,34 @@
 Shareaprayer::Application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-
+  if !Rails.env.production?  
+    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  end
+  
   constraints(subdomain: /^(|www)$/) do
     root to: 'pages#show', id: 'home'
 
     get '/:id', to: 'pages#show', as: :static
   end
-  
-  constraints(subdomain: /^(|live)$/) do
-    scope module: "live" do
-      get '/' => 'recent#index', as: "recent"
-      get '/:id' => 'church#show', as: "church"
 
-      resources :requests, only: [:create]
-      resources :church, only: [], as: "church" do
-        member do
-          get 'join'
-          get 'leave'
+  if !Rails.env.production?  
+    constraints(subdomain: /^(|live)$/) do
+      scope module: "live" do
+        get '/' => 'recent#index', as: "recent"
+        get '/:id' => 'church#show', as: "church"
+
+        resources :requests, only: [:create]
+        resources :church, only: [], as: "church" do
+          member do
+            get 'join'
+            get 'leave'
+          end
         end
       end
     end
-  end
 
-  constraints(ChurchSubdomain) do
-    get '/' => redirect { |params, request|
-      "http://live.#{request.domain}/#{request.subdomain}"
-    }
+    constraints(ChurchSubdomain) do
+      get '/' => redirect { |params, request|
+        "http://live.#{request.domain}/#{request.subdomain}"
+      }
+    end
   end
 end
